1b46245ee6de0539cbe52210346552cb36795fd1
root <root@ip-172-31-9-251.ap-southeast-2.compute.internal>
2017-12-09 05:53:04 +0000

    adding deplyment steps puppet manifest

--- puppet/manifests/commtest.pp
+++ puppet/manifests/commtest.pp
@@ -1,9 +1,39 @@
 node default {
- include python
- include pip
+  include python
+  include pip
 
- pip::install { 'virtualenv':
-  package        => 'virtualenv', 
-  ensure         => present,  
- }
+# pip::install { 'virtualenv':
+#  package        => 'virtualenv', 
+#  ensure         => present,  
+# }
+  
+  pip::install { 'virtualenv':
+    package        => 'virtualenv',
+    ensure         => present,
+  } ->
+
+  file { '/opt/app':
+    ensure => 'directory',
+  } ->
+
+  exec { 'untar':
+    command     => '/bin/tar -zxf /tmp/flask/packages/system-engineer.tar.gz -C /opt/app/',
+  } ->
+
+  exec { 'appdir':
+    cwd => '/opt/app/system-engineer',
+    command => '/bin/virtualenv .',
+  } ->
+
+  exec { 'installpackages':
+    cwd => '/opt/app/system-engineer',
+    command => '/bin/pip install -r requirements.txt',
+  } ->
+
+  exec { 'deployapp':
+    cwd => '/opt/app/system-engineer',
+    command => '/bin/python app/app.py &',
+  }
+    
+
 }
